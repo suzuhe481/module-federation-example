4eda371a523490a2f349ab87264e3c87bb4761ad
Hector <baseballhector@gmail.com>
2026-04-20 16:27:47 -0400

    feat: update config to use zephyr plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
+import { withZephyr } from "vite-plugin-zephyr";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), withZephyr()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
